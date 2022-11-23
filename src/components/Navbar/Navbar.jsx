5b8fe8225007e293062c88cbd86ad03545fe7a00
Marcus Paulo <marcuspaulo.morenomf@gmail.com>
2022-11-23 17:19:59 -0300

    funcionalidade de pesquisar funcionando

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Header } from './Navbar.styled';
 import logo from '../../assets/logo.svg';
 import {FaShoppingCart, FaSearch} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { mudarBusca, resetarBusca } from '../../store/reducers/busca';
+
 
 export const Navbar = () => {
+
+  const [valueInput, setValueInput] = useState('')
+
+  const buscar = useSelector(state=> state.busca)
+  const dispatch = useDispatch()
+  const location = useLocation()
+
+  useEffect(()=>{
+    dispatch(resetarBusca())
+  },[location.pathname, dispatch])
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    mudarBusca(valueInput)
+  }
+
   return (
     <Header>
       <div className='logoContainer'>
@@ -12,10 +33,13 @@ export const Navbar = () => {
         <Link to={'/'} className='home'>Home</Link>
       </div>
       <div className='pesquisaContainer'>
-        <div className='Pesquisar'>
-        <input type="text" placeholder='O que você procura?...'/>
-        <i className='iconeProcura'><FaSearch/></i>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className='Pesquisar'>
+            <input type="text"placeholder='O que você procura?...' onChange={e => setValueInput(e.target.value)}/>
+
+            <button className='iconeProcura'><FaSearch/></button>
+          </div>
+        </form>
         <Link to={'/carrinho'} className='compras'><FaShoppingCart/></Link>
       </div>
     </Header>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import categoriesSlice from './reducers/categories';
 import itemsSlice from './reducers/items';
+import buscaSlice from './reducers/busca'
 
 const store = configureStore({
     reducer: {
         categorias: categoriesSlice,
         itens: itemsSlice,
+        busca: buscaSlice
     }
 });
 
