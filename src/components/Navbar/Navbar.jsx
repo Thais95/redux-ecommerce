f6ebe5a2340e36cf43474127425953d1a3e5ddac
Thais95 <thaiscdaraujo95@gmail.com>
2022-11-23 19:07:30 -0300

    ajustando funcionalidade de pesquisa

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from './Navbar.styled';
 import logo from '../../assets/logo.svg';
-import {FaShoppingCart, FaSearch} from 'react-icons/fa';
+import { FaShoppingCart, FaSearch } from 'react-icons/fa';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { mudarBusca, resetarBusca } from '../../store/reducers/busca';
@@ -13,26 +13,42 @@ export const Navbar = () => {
   const dispatch = useDispatch()
   const location = useLocation()
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(resetarBusca())
-  },[location.pathname, dispatch])
+  }, [location.pathname, dispatch])
 
-  function handleSubmit() {
+  const handleChange = event => {
+    setValueInput(event.target.value);
+  }
+
+  const handleClick = () => {
     dispatch(mudarBusca(valueInput))
+    setValueInput('');
   }
 
   return (
     <Header>
       <div className='logoContainer'>
+
         <Link to={'/'}><img className='logo' src={logo} alt="Logo" /></Link>
         <Link to={'/'} className='home'>Home</Link>
+
       </div>
       <div className='pesquisaContainer'>
         <div className='Pesquisar'>
-          <input type="text" placeholder='O que você procura?...' onChange={e => setValueInput(e.target.value)} />
-          <i onClick={handleSubmit} className='iconeProcura'><FaSearch/></i>
+
+          <input type="text"
+            placeholder='O que você procura?...'
+            value={valueInput}
+            onChange={handleChange} />
+          <i
+            onClick={handleClick}
+            className='iconeProcura'>
+            <FaSearch />
+          </i>
+
         </div>
-        <Link to={'/carrinho'} className='compras'><FaShoppingCart/></Link>
+        <Link to={'/carrinho'} className='compras'><FaShoppingCart /></Link>
       </div>
     </Header>
   )
